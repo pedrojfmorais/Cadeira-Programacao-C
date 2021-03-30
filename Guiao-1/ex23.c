2633fb8f86e498774ddb687cfd242ee238752b4f
Pedro Morais <pedrojfmorais@hotmail.com>
2021-03-30 23:24:55 +0100

    guiao 1 - 23

--- Guiao-1/ex23.c
+++ Guiao-1/ex23.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    
+    if (argc != 3)
+    {
+        printf("Mais ou menos que duas palavras. \n");
+        return 0;
+    }
+    
+    if (strlen(argv[1]) != strlen(argv[2]))
+    {
+        printf("As palavras tem tamanhos diferentes. \n");
+        return 0;
+    }
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        printf("%c%c", argv[1][i], argv[2][i]);
+    }
+    
+    printf("\n");
+
     return 0;
 }
