0df5caaa3075b98fd47f63329b4fde0bb8406ca2
Pedro Morais <pedrojfmorais@hotmail.com>
2021-05-08 00:37:37 +0100

    testeClion

---
+++ testeClion/cmake-build-debug/CMakeFiles/testeClion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testeClion.dir/main.c.obj"
+  "libtesteClion.dll.a"
+  "testeClion.exe"
+  "testeClion.exe.manifest"
+  "testeClion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testeClion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
